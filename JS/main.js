75cd310ccdf696e73e46566e4e1c0a3b8dd31947
mativazq <mativazquezfotografia@hotmail.com>
2022-08-31 22:56:03 -0300

    nuevo JS

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+/* let nombreIngresado = prompt('ingrese nombre de usuario');
+console.log (nombreIngresado);
+alert ('Aloha ' + nombreIngresado);
+
+
+let edad = prompt ('Aca va tu edad');
+let nacimiento = 2022 - edad ;
+alert ('sos categoria' + nacimiento);
+ */
+
+let numeroIngresado = prompt('Ingrese un numero');
+
+    if (numeroIngresado > 1000) {
+        alert ('el numero es mayor a 1000');
+    } else {
+        alert ('el numero es menor a 1000');
+    }
