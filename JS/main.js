628ed86100f5e0d0a791ad7464a282f0a803e4be
mativazq <mativazquezfotografia@hotmail.com>
2022-09-06 21:25:38 -0300

    desafio complementario

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,6 @@
-/* let nombreIngresado = prompt('ingrese nombre de usuario');
-console.log (nombreIngresado);
-alert ('Aloha ' + nombreIngresado);
+let nombre = prompt ('aca va tu nombre');
 
-
-let edad = prompt ('Aca va tu edad');
-let nacimiento = 2022 - edad ;
-alert ('sos categoria' + nacimiento);
- */
-
-let numeroIngresado = prompt('Ingrese un numero');
-
-    if (numeroIngresado > 1000) {
-        alert ('el numero es mayor a 1000');
-    } else {
-        alert ('el numero es menor a 1000');
-    }
+while (nombre != 'esc') {
+    alert ('Vos te llamas ' + nombre);
+    nombre = prompt ('escribi otro nombre');
+}
