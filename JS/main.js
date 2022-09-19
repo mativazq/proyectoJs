b297f78b941d094160c84e4e485364b0323b01ab
mativazq <mativazquezfotografia@hotmail.com>
2022-09-19 16:33:58 -0300

    correcion error entregable

--- JS/main.js
+++ JS/main.js
@@ -10,8 +10,8 @@ function menuDelicias(){
     let delicias;
 
     do{
-        delicias = prompt("Que postre vas a elegir? : \n1.- Chocotorta\n2.- Torta Bombon\n3.- Merengada" )
-    }while(delicias != 1 && delicias !=2 && delicias !=3)
+        delicias = prompt("Que postre vas a elegir? : \n1.- Chocotorta\n2.- Torta Bombon\n3.- Merengada \n4.- Cocada" )
+    }while(delicias !=1 && delicias !=2 && delicias !=3 && delicias !=4)
 
     switch(delicias){
         case "1":
@@ -20,6 +20,8 @@ function menuDelicias(){
             return "Torta Bombon";
         case "3":
             return "Merengada";
+        case "4":
+            return "Cocada";
     }
 }
 
@@ -30,11 +32,13 @@ function precioTortas(torta) {
         return 500;
     }else if(torta === "Merengada"){
         return 700
+    }else if(torta === "Cocada"){
+        return 600
     }
 }
 
 function cobrarTorta (torta,precio){
-alert("Usted eligio : "+torta.toUpperCase()+".\nEl precio de la torta es de $"+precio);
+alert("Usted eligio : "+torta+".\nEl precio de la torta es de $"+precio);
 let pago = prompt("Con cuanto desea abonar?");
 
 if(precio < pago ){
@@ -45,4 +49,4 @@ if(precio < pago ){
 bienvenida();
 let deliciaSeleccionada =  menuDelicias();
 let precioDelicia = precioTortas(deliciaSeleccionada);
-cobrarTorta(nombreTorta,deliciaSeleccionada)
+cobrarTorta(deliciaSeleccionada,precioDelicia);
